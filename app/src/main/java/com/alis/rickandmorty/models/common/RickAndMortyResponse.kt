39e896625e25f1830b18952c902c892e94799a9b
macbookpro <abenov2005.uzer@gmail.com>
2021-03-10 17:59:46 +0600

    minor refactoring

--- app/src/main/java/com/alis/rickandmorty/models/common/RickAndMortyResponse.kt
+++ app/src/main/java/com/alis/rickandmorty/models/common/RickAndMortyResponse.kt
@@ -2,7 +2,7 @@ package com.alis.rickandmorty.models.common
 
 import com.google.gson.annotations.SerializedName
 
-data class RickAndMortyResponse<T : Any>(
+data class RickAndMortyResponse<T>(
     @SerializedName("info")
     val info: Info,
     @SerializedName("results")
