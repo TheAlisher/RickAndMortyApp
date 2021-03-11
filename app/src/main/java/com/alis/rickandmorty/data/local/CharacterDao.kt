300d9b23cde5eaa73c3687de09e2d4ab5966f6ce
macbookpro <abenov2005.uzer@gmail.com>
2021-03-11 23:34:22 +0600

    added -> ConflictStrategy

--- app/src/main/java/com/alis/rickandmorty/data/local/CharacterDao.kt
+++ app/src/main/java/com/alis/rickandmorty/data/local/CharacterDao.kt
@@ -2,13 +2,14 @@ package com.alis.rickandmorty.data.local
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.alis.rickandmorty.models.character.Character
 
 @Dao
 interface CharacterDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(characters: MutableList<Character>)
 
     @Query("SELECT * FROM character")
