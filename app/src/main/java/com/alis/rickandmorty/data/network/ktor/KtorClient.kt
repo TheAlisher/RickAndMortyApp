40f320705580bfb355172a698af49dddc5f24148
macbookpro <abenov2005.uzer@gmail.com>
2021-03-14 04:38:36 +0600

    added -> Ktor's OkHttp

--- app/src/main/java/com/alis/rickandmorty/data/network/ktor/KtorClient.kt
+++ app/src/main/java/com/alis/rickandmorty/data/network/ktor/KtorClient.kt
@@ -2,27 +2,42 @@ package com.alis.rickandmorty.data.network.ktor
 
 import com.alis.rickandmorty.constants.NetworkConstants
 import io.ktor.client.*
-import io.ktor.client.engine.android.*
+import io.ktor.client.engine.okhttp.*
 import io.ktor.client.features.*
 import io.ktor.client.features.json.*
-import io.ktor.client.features.logging.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
 
 class KtorClient {
 
-    private val client = HttpClient {
+    private val client = HttpClient(OkHttp) {
         defaultRequest {
             host = NetworkConstants.HOST
             url {
                 protocol = URLProtocol.HTTPS
             }
         }
+
         install(JsonFeature) {
             serializer = GsonSerializer()
         }
-        install(Logging) {
-            level = LogLevel.BODY
+
+        engine {
+            addInterceptor(provideLoggingInterceptor())
+
+            config {
+                connectTimeout(30, TimeUnit.SECONDS)
+                readTimeout(30, TimeUnit.SECONDS)
+                writeTimeout(30, TimeUnit.SECONDS)
+            }
+        }
+    }
+
+    private fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
         }
     }
 
