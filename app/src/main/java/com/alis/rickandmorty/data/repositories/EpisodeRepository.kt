162aeb2536873b1946dc5d8abdcb62a1d8529dd7
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-06-19 19:49:31 +0600

    refactoring

--- app/src/main/java/com/alis/rickandmorty/data/repositories/EpisodeRepository.kt
+++ app/src/main/java/com/alis/rickandmorty/data/repositories/EpisodeRepository.kt
@@ -1,22 +1,21 @@
 package com.alis.rickandmorty.data.repositories
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
+import com.alis.rickandmorty.base.BaseRepository
 import com.alis.rickandmorty.data.network.Resource
 import com.alis.rickandmorty.data.network._Resource
 import com.alis.rickandmorty.data.network.ktor.EpisodeApiService
+import com.alis.rickandmorty.models.common.RickAndMortyResponse
+import com.alis.rickandmorty.models.episode.Episode
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 class EpisodeRepository @Inject constructor(
     private val episodeApiService: EpisodeApiService
-) {
+) : BaseRepository() {
 
-    fun fetchEpisodes() = liveData(Dispatchers.IO) {
-        emit(_Resource.Loading())
-        try {
-            emit(_Resource.Success(data = episodeApiService.fetchEpisodes()))
-        } catch (E: Exception) {
-            emit(_Resource.Error(data = null, message = E.message ?: "Error Occurred!"))
-        }
+    fun fetchEpisodes(): LiveData<_Resource<RickAndMortyResponse<Episode>>> {
+        return doRequest { episodeApiService.fetchEpisodes() }
     }
 }
