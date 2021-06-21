fca3583cf965aaaa5a794ce4ded94da50ab9defc
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-06-22 01:41:53 +0600

    removed unnecessary

--- app/src/main/java/com/alis/rickandmorty/data/repositories/EpisodeRepository.kt
+++ app/src/main/java/com/alis/rickandmorty/data/repositories/EpisodeRepository.kt
@@ -1,11 +1,7 @@
 package com.alis.rickandmorty.data.repositories
 
 import com.alis.rickandmorty.base.BaseRepository
-import com.alis.rickandmorty.data.network._Resource
 import com.alis.rickandmorty.data.network.ktor.EpisodeApiService
-import com.alis.rickandmorty.models.common.RickAndMortyResponse
-import com.alis.rickandmorty.models.episode.Episode
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class EpisodeRepository @Inject constructor(
