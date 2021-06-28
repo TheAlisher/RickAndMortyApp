f27e42047039aeb320791588584a9402e67814b1
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-06-29 02:17:47 +0600

    deleted DetailFragment

--- app/src/main/java/com/alis/rickandmorty/ui/fragments/detail/DetailViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.alis.rickandmorty.ui.fragments.detail
-
-import com.alis.rickandmorty.base.BaseViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class DetailViewModel @Inject constructor(
-) : BaseViewModel()
