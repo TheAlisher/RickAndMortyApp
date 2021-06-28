ee0a9a2e1ab15a088b87845297b82e151654e0db
Alisher Abenov <60399817+TheAlisher@users.noreply.github.com>
2021-06-29 04:27:39 +0600

    decrement version

--- app/src/main/java/com/alis/rickandmorty/data/db/room/RoomDatabase.kt
+++ app/src/main/java/com/alis/rickandmorty/data/db/room/RoomDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.TypeConverters
 import com.alis.rickandmorty.data.db.room.daos.CharacterDao
 import com.alis.rickandmorty.models.character.Character
 
-@Database(entities = [Character::class], version = 2)
+@Database(entities = [Character::class], version = 1)
 @TypeConverters(Converters::class)
 abstract class RoomDatabase : RoomDatabase() {
 
