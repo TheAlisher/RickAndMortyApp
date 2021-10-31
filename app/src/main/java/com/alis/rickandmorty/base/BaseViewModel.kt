554afa9664b3ee3bbcbc0fd68c374b3d9026dc38
TheAlisher <allisher.abenov@gmail.com>
2021-10-31 15:31:00 +0600

    minor refactoring

--- app/src/main/java/com/alis/rickandmorty/base/BaseViewModel.kt
+++ app/src/main/java/com/alis/rickandmorty/base/BaseViewModel.kt
@@ -2,28 +2,31 @@ package com.alis.rickandmorty.base
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.alis.rickandmorty.common.resource.Resource
 import com.alis.rickandmorty.presentation.state.UIState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 abstract class BaseViewModel : ViewModel() {
 
-    protected suspend fun <T> subscribeTo(
+    protected fun <T> subscribeTo(
         state: MutableLiveData<UIState<T>>,
         request: () -> Flow<Resource<T>>
     ) {
-        request().collect {
-            when (it) {
-                is Resource.Loading -> {
-                    state.value = UIState.Loading()
-                }
-                is Resource.Error -> it.message?.let { error ->
-                    state.value = UIState.Error(error)
-                }
-
-                is Resource.Success -> it.data?.let { data ->
-                    state.value = UIState.Success(data)
+        viewModelScope.launch {
+            request().collect {
+                when (it) {
+                    is Resource.Loading -> {
+                        state.value = UIState.Loading()
+                    }
+                    is Resource.Error -> it.message?.let { error ->
+                        state.value = UIState.Error(error)
+                    }
+                    is Resource.Success -> it.data?.let { data ->
+                        state.value = UIState.Success(data)
+                    }
                 }
             }
         }
